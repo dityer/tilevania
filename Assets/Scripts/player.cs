856287bd680fc6f8ae6bd30ec43ce1b80f397ead
Adithya Chetty <adithya.chetty@gmail.com>
2020-04-01 15:22:30 -0700

    i died but that means i completed player death which is fun

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -9,6 +9,7 @@ public class player : MonoBehaviour {
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
 
     // State
     bool isAlive = true;
@@ -16,7 +17,7 @@ public class player : MonoBehaviour {
     // Cached component references
     Rigidbody2D myRigidBody;
     Animator myAnimator;
-    CapsuleCollider2D myBodyCollider2D;
+    CapsuleCollider2D myBodyCollider;
     BoxCollider2D myFeet;
     float gravityScaleAtStart;
 
@@ -24,9 +25,10 @@ public class player : MonoBehaviour {
     // Message then methods
     void Start()
     {
+
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myBodyCollider2D = GetComponent<CapsuleCollider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
         myFeet = GetComponent<BoxCollider2D>();
         gravityScaleAtStart = myRigidBody.gravityScale;
     }
@@ -34,10 +36,13 @@ public class player : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (!isAlive) { return; }
+
         Run();
         Jump();
         FlipSprite();
         ClimbLadder();
+        Die();
     }
 
     private void Run()
@@ -82,6 +87,17 @@ public class player : MonoBehaviour {
         {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
             myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+
+    private void Die()
+    {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
         }
     }
 
